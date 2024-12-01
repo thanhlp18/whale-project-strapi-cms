ab78a74da752db4505ba40d04e62f321ba30fb7d
Thanh Le <thanh.le@qode.world>
2024-12-01 15:34:13 +0700

    Chore: Update Schema for description

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -452,7 +452,7 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::blog.blog'> &
       Schema.Attribute.Private;
